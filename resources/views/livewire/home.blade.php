5eefe5e88d6054c1b9612f4b616a3a58fdc9afb5
John Vo <vojohn95@gmail.com>
2023-12-08 14:01:30 -0600

    fx
    fx

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -83,7 +83,7 @@
                                 </svg>
                                 Configuración
                             </div>
-                            @if ($currentStep = 0)
+                            @if ($currentStep = 1)
                                 <div class="col-sm pulse-effect text-dark ">
                                 @else
                                     <div class="col-sm">
@@ -95,39 +95,42 @@
                             </svg>
                             Ataque en progreso
                         </div>
-                        <div class="col-sm">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                class="bi bi-3-circle" viewBox="0 0 16 16">
-                                <path
-                                    d="M7.918 8.414h-.879V7.342h.838c.78 0 1.348-.522 1.342-1.237 0-.709-.563-1.195-1.348-1.195-.79 0-1.312.498-1.348 1.055H5.275c.036-1.137.95-2.115 2.625-2.121 1.594-.012 2.608.885 2.637 2.062.023 1.137-.885 1.776-1.482 1.875v.07c.703.07 1.71.64 1.734 1.917.024 1.459-1.277 2.396-2.93 2.396-1.705 0-2.707-.967-2.754-2.144H6.33c.059.597.68 1.06 1.541 1.066.973.006 1.6-.563 1.588-1.354-.006-.779-.621-1.318-1.541-1.318Z" />
-                                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8" />
-                            </svg>
-                            Resultado
-                        </div>
-                        <div class="col-sm">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                class="bi bi-4-circle" viewBox="0 0 16 16">
-                                <path
-                                    d="M7.519 5.057c.22-.352.439-.703.657-1.055h1.933v5.332h1.008v1.107H10.11V12H8.85v-1.559H4.978V9.322c.77-1.427 1.656-2.847 2.542-4.265ZM6.225 9.281v.053H8.85V5.063h-.065c-.867 1.33-1.787 2.806-2.56 4.218Z" />
-                                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8" />
-                            </svg>
-                            Reporte
-                        </div>
+                        @if ($currentStep = 2)
+                            <div class="col-sm pulse-effect text-dark ">
+                            @else
+                                <div class="col-sm">
+                        @endif
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                            class="bi bi-3-circle" viewBox="0 0 16 16">
+                            <path
+                                d="M7.918 8.414h-.879V7.342h.838c.78 0 1.348-.522 1.342-1.237 0-.709-.563-1.195-1.348-1.195-.79 0-1.312.498-1.348 1.055H5.275c.036-1.137.95-2.115 2.625-2.121 1.594-.012 2.608.885 2.637 2.062.023 1.137-.885 1.776-1.482 1.875v.07c.703.07 1.71.64 1.734 1.917.024 1.459-1.277 2.396-2.93 2.396-1.705 0-2.707-.967-2.754-2.144H6.33c.059.597.68 1.06 1.541 1.066.973.006 1.6-.563 1.588-1.354-.006-.779-.621-1.318-1.541-1.318Z" />
+                            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8" />
+                        </svg>
+                        Resultado
+                    </div>
+                    <div class="col-sm">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                            class="bi bi-4-circle" viewBox="0 0 16 16">
+                            <path
+                                d="M7.519 5.057c.22-.352.439-.703.657-1.055h1.933v5.332h1.008v1.107H10.11V12H8.85v-1.559H4.978V9.322c.77-1.427 1.656-2.847 2.542-4.265ZM6.225 9.281v.053H8.85V5.063h-.065c-.867 1.33-1.787 2.806-2.56 4.218Z" />
+                            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8" />
+                        </svg>
+                        Reporte
                     </div>
                 </div>
-                <div class="container mt-2">
-                    <div class="row">
-                        <div class="progress-wrapper">
-                            <div class="progress-info">
-                                <div class="progress-percentage">
-                                    <span class="text-sm font-weight-bold">Progreso del ataque:
-                                        {{ $loader_timer }}%</span>
-                                </div>
+            </div>
+            <div class="container mt-2">
+                <div class="row">
+                    <div class="progress-wrapper">
+                        <div class="progress-info">
+                            <div class="progress-percentage">
+                                <span class="text-sm font-weight-bold">Progreso del ataque:
+                                    {{ $loader_timer }}%</span>
                             </div>
-                            <div class="progress">
-                                <div class="progress-bar bg-gradient-primary" role="progressbar" aria-valuenow="60"
-                                    aria-valuemin="0" aria-valuemax="100" style="width: {{ $loader_timer }}%;">
-                                </div>
+                        </div>
+                        <div class="progress">
+                            <div class="progress-bar bg-gradient-primary" role="progressbar" aria-valuenow="60"
+                                aria-valuemin="0" aria-valuemax="100" style="width: {{ $loader_timer }}%;">
                             </div>
                         </div>
                     </div>
@@ -135,6 +138,7 @@
             </div>
         </div>
     </div>
+</div>
 
 </div>
 
